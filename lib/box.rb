48971ff42f4d9e68bd6d36c760c61bc9a9c9148c
dan.pan <dan.pan@student.com>
2018-12-29 18:51:31 +0800

    correct format

--- lib/box.rb
+++ lib/box.rb
@@ -5,25 +5,25 @@ class Box
 
   def initialize key
     key_env = ENV[key]
-		if key_env.nil?
-			puts "There's no environment variable #{key}..."
-			return
-		end
-		key = Base64.decode64(key_env)
-		@box = RbNaCl::SimpleBox.from_secret_key(key)
+    if key_env.nil?
+      puts "There's no environment variable #{key}..."
+      return
+    end
+    key = Base64.decode64(key_env)
+    @box = RbNaCl::SimpleBox.from_secret_key(key)
   end
 
-	def file_encrypt file
+  def file_encrypt file
     plaintext = File.read(file)
     ciphertext = @box.encrypt(plaintext)
     enc_file = file + '.enc'
     File.write(enc_file, Base64.encode64(ciphertext))
-	end
+  end
 
   def file_decrypt file
     ciphertext = File.read(file)
     plaintext = @box.decrypt(Base64.decode64(ciphertext))
     plain_file = file[0..-5]
     File.write(plain_file, plaintext)
-	end
+  end
 end
