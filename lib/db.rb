cb1f3e9dd31f97df942c75349238f02a459969f4
dan.pan <dan.pan@student.com>
2019-08-07 18:18:02 +0800

    MOON-136 工具库：重构连接池Pool，直接换回DB对象或者SSH对象

--- lib/db.rb
+++ lib/db.rb
@@ -1,16 +1,15 @@
-require 'accessors'
-
-class DBFactory
-  extend Accessors
-
-  attr_accessor :db
+class Database
 
   def initialize db
-    @db = db
+    @@db = db
+  end
+
+  def get_db
+    return @@db
   end
 
   def query sql
-    result = db[sql].all
+    result = @@db[sql].all
     return result
   end
 
@@ -21,7 +20,78 @@ class DBFactory
     end
   end
 
+  def query_duration sql, type = true, waiting_time = 10
+    start = Time.now
+    if type
+      result = query(sql, waiting_time)
+    else
+      result = query_empty(sql, waiting_time)
+    end
+    finish = Time.now
+    msecs = (finish - start) * 1000.0
+    hash = {'result' => result, 'duration' => msecs}
+    return hash
+  end
+
+  ## Expect to return data for the query.
+  ## If the result is empty then it will be called again until waiting_time reach.
+  def query_wait sql, waiting_time = 10
+    result = @@db[sql].all
+    if result.length == 0
+      if waiting_time != 0
+        sleep 1
+        result = query(sql, waiting_time - 1)
+      end
+    end
+    return result
+  end
+
+  ## Expect to return empty for the query.
+  ## If the result is not empty then it will be called again until waiting_time reach.
+  def query_empty sql, waiting_time = 10
+    result = @@db[sql].all
+    if result.length != 0
+      if waiting_time != 0
+        sleep 1
+        result = query(sql, waiting_time - 1)
+      end
+    end
+    return result
+  end
+
   def insert sql
-    db[sql]
+    @@db[sql]
+  end
+
+  def insert_by_data data, table
+    sql = "insert into #{table} "
+    case data
+      when Array
+        data.each do |d|
+          insert_by_data(d, table)
+        end
+      when Hash
+        columns = data.keys.to_s.gsub('[','(').gsub(']',')').gsub('"','')
+        values = data.values.to_s.gsub('[','(').gsub(']',')').gsub('nil','NULL')
+        sql = sql + columns + " values " + values
+        query(sql,0)
+     end
+  end
+
+  def update data, table, condition
+    sql = "update #{table} set"
+    data.each do |k,v|
+      v = v.to_json if v.is_a?(Hash)
+      if !!v == v
+        sql = "#{sql} #{k}=#{v},"
+      else
+        sql = "#{sql} #{k}='#{v}',"
+      end
+    end
+    sql = sql[0..-2] + " where"
+    condition.each do |k,v|
+      sql = "#{sql} #{k} = '#{v}' and"
+    end
+    query(sql[0..-4],0)
   end
 end
--- lib/pool.rb
+++ lib/pool.rb
@@ -1,56 +1,60 @@
 require 'mysql2'
 require 'sequel'
 require 'net/ssh/gateway'
+require 'psych'
+require './lib/db'
 
 class Pool
 
-  attr_accessor :db_pool, :ssh_pool, :file_path
-
   def initialize file_path
-    @db_pool = {}
-    @ssh_pool = {}
-    @file_path = file_path
+    @@db_pool = {}
+    @@ssh_pool = {}
+    @@file = Psych.load_file(file_path)
   end
 
-  def use(params ={})
+  def use(params = {})
     ssh_name = params.fetch(:ssh, false)
     db_name = params.fetch(:db, false)
-    if db_pool[db_name].nil? && db_name
-      ssh_key = fetch_ssh_config db_name
-      unless ssh_key.nil?
-        ssh = create_or_use_ssh ssh_key
-        port = forward_port ssh, ssh_key
-        db = connect_database db_name, port
-        db_pool[db_name] = db
-      else
-        db = connect_database_directly db_name
-        db_pool[db_name] = db
-      end
+    if db_name
+      return get_db db_name
     elsif ssh_name
-      create_or_use_ssh ssh_name
+      return get_ssh ssh_name
     else
-      db_pool[db_name]
+      fail "Please provide :ssh or :db"
     end
   end
 
-  def create_or_use_ssh name
-    if ssh_pool[name].nil?
+  def get_db name
+    if @@db_pool[name].nil?
+      db_conf =  @@file[name]
+      if db_conf['proxy'].nil?
+        db = connect_database db_conf
+      else
+        db = connect_database_by_proxy db_conf
+      end
+      @@db_pool[name] = Database.new(db)
+    end
+    return @@db_pool[name]
+  end
+
+  def get_ssh name
+    if @@ssh_pool[name].nil?
       ssh = connect_remote_server name
-      ssh_pool[name] = ssh
+      @@ssh_pool[name] = ssh
     else
-      ssh_pool[name]
+      @@ssh_pool[name]
     end
   end
 
-  def connect_database name, port=false
-    database_file = YAML.load_file(file_path)
-    db_conf = database_file[name]
-    db_conf = db_conf.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-    db_conf = db_conf.merge(:port => port) if port
-    db = get_ready_for_database db_conf
+  def connect_database_by_proxy conf
+    port = forward_port conf['proxy']
+    connect_database conf, port
   end
 
-  def get_ready_for_database conf
+  def connect_database conf, port = false
+    conf.delete('proxy') if port
+    conf = conf.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+    conf = conf.merge(:port => port) if port
     begin
       Sequel.connect(conf)
     rescue
@@ -59,32 +63,23 @@ class Pool
   end
 
   def connect_remote_server name
-    database_file = YAML.load_file(file_path)
-    ssh_conf = database_file[name]
-    ssh_conf = ssh_conf.delete_if { |key,value| key == 'database'}
-    ssh = get_ready_for_ssh ssh_conf
-    return ssh
-  end
-
-  def get_ready_for_ssh conf
-    host = conf.delete('host')
-    user = conf.delete('user')
-    options = conf.delete_if {|key,value| key == 'host' && 'user'}
-    options = options.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+    ssh_conf = @@file[name]
+    host = ssh_conf.delete('host')
+    user = ssh_conf.delete('user')
+    options = ssh_conf.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
     begin
       Net::SSH::Gateway.new(host, user, options)
     rescue
-      fail "ssh configuration \n #{conf} \n is not correct, please double check"
+      fail "ssh configuration \n #{ssh_conf} \n is not correct, please double check"
     end
   end
 
-  def forward_port ssh, name
-    database_file = YAML.load_file(file_path)
-    ssh_conf = database_file[name]
-    new_conf = ssh_conf.delete('database')
-    host = new_conf.delete('host')
-    remote_port = new_conf.delete('remote_port')
-    local_port = new_conf.delete('local_port')
+  def forward_port conf
+    ssh = get_ssh conf['ssh']
+    host = conf['database']['host']
+    remote_port = conf['database']['remote_port']
+    local_port = conf['database']['local_port']
+    # puts "forward remote port #{remote_port} to local port #{local_port}"
     begin
       ssh.open(host, remote_port, local_port)
     rescue Errno::EADDRINUSE
@@ -92,18 +87,5 @@ class Pool
     rescue
       fail "fail to forward remote port #{remote_port} to local_port #{local_port}"
     end
-  end
-
-  def fetch_ssh_config db_name
-    database_file = YAML.load_file(file_path)
-    db_conf  = database_file[db_name]
-    ssh_key = db_conf.delete('ssh')
-  end
-
-  def connect_database_directly db_name
-    database_file = YAML.load_file(file_path)
-    db_conf  = database_file[db_name]
-    db_conf = db_conf.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-    db = Sequel.connect(db_conf)
   end
 end
