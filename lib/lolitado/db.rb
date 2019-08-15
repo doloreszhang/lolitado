34e507c2d3d50f41d4f5be65d2a24908bc3d6544
dan.pan <dan.pan@student.com>
2019-08-15 12:12:33 +0800

    Fix bugs for db

--- lib/lolitado/db.rb
+++ lib/lolitado/db.rb
@@ -6,24 +6,31 @@ module Lolitado
     def initialize db
       @db = db
     end
-    
-    
+
+    # Return the data by sql.
+    # @param [String] sql the sql you wanna query
     def query sql
       result = db[sql].all
       return result
     end
 
+    # Return the data by sql.
+    # @param [String] sql the sql you wanna query, the multiple sql should be split by ;
     def multiple_query sql
       splited_sql = sql.split(';')
       splited_sql.each do |each_sql|
         query(each_sql)
       end
     end
-    
+
+    # Return the data and execute duration by sql.
+    # @param [String] sql the sql you wanna query
+    # @param [Boolean] type the default value is true that the expected result is not empty, otherwise, the expected result is empty
+    # @param [Number] waiting_time the default value is 10 that maxium execute duration is 10 second
     def query_duration sql, type = true, waiting_time = 10
       start = Time.now
       if type
-        result = query(sql, waiting_time)
+        result = query_wait(sql, waiting_time)
       else
         result = query_empty(sql, waiting_time)
       end
@@ -33,21 +40,23 @@ module Lolitado
       return hash
     end
 
-    ## Expect to return data for the query.
-    ## If the result is empty then it will be called again until waiting_time reach.
+    # Expect to return data for the query. If the result is empty then it will be called again until waiting_time reach.
+    # @param [String] sql the sql you wanna query
+    # @param [Number] waiting_time the default value is 10 that maxium execute duration is 10 second
     def query_wait sql, waiting_time = 10
       result = db[sql].all
-      if result.length == 0
+      if result.empty?
         if waiting_time != 0
           sleep 1
-          result = query(sql, waiting_time - 1)
+          result = query_wait(sql, waiting_time - 1)
         end
       end
       return result
     end
 
-    ## Expect to return empty for the query.
-    ## If the result is not empty then it will be called again until waiting_time reach.
+    # Expect to return empty for the query. If the result is not empty then it will be called again until waiting_time reach.
+    # @param [String] sql the sql you wanna query
+    # @param [Number] waiting_time the default value is 10 that maxium execute duration is 10 second
     def query_empty sql, waiting_time = 10
       result = db[sql].all
       if result.length != 0
@@ -59,10 +68,15 @@ module Lolitado
       return result
     end
 
-    def insert sql
+    # Execute by sql, the sql is normally update or insert.
+    # @param [String] sql the sql you wanna execute
+    def execute sql
       db[sql]
     end
 
+    # Insert data by data.
+    # @param [String] data the data wanna insert which is array or hash
+    # @param [String] table the table wanna insert
     def insert_by_data data, table
       sql = "insert into #{table} "
       case data
@@ -74,11 +88,15 @@ module Lolitado
           columns = data.keys.to_s.gsub('[','(').gsub(']',')').gsub('"','')
           values = data.values.to_s.gsub('[','(').gsub(']',')').gsub('nil','NULL')
           sql = sql + columns + " values " + values
-          query(sql,0)
+          query(sql)
       end
     end
 
-    def update data, table, condition
+    # Update data by data.
+    # @param [Hash] data the data wanna update which is hash
+    # @param [String] table the table for the update
+    # @param [Hash] condition the condition for the update
+    def update data, table, condition = {}
       sql = "update #{table} set"
       data.each do |k,v|
         v = v.to_json if v.is_a?(Hash)
@@ -92,7 +110,7 @@ module Lolitado
       condition.each do |k,v|
         sql = "#{sql} #{k} = '#{v}' and"
       end
-      query(sql[0..-4],0)
+      query(sql[0..-4])
     end
   end
 end
